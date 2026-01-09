7cf62f763d3e569402a1a1b75247d261d6d7f0a0
zzh <57039182+zzhhao2008@users.noreply.github.com>
2026-01-10 00:58:24 +0800

    LVGL移植

--- luadb/main.lua
+++ luadb/main.lua
@@ -9,89 +9,80 @@ if wdt then
     sys.timerLoopStart(wdt.feed, 3000) -- 3s喂一次狗
 end
 
-ft6636u = require "ft6636u"
 require "lcdd"
 
-local colorList = {0xF800, 0x07E0, 0x001F, 0xFFFF, 0x0000, 0xF81F, 0xFFE0, 0x07FF, 0x03FF, 0x01FF, 0x07F0}
-nowColor = 1
-
-vButton = {{
-    x = 0,
-    y = 0,
-    w = 30,
-    h = 30
-}, {
-    x = 40,
-    y = 0,
-    w = 30,
-    h = 30
-}}
-
 sys.taskInit(function()
-    ft6636u.init({
-        sda = 11,
-        scl = 9,
-        fre = i2c.FAST,
+    local config = {
+        sda = 11, -- SDA引脚
+        scl = 9, -- SCL引脚
+        rst = 10, -- RST引脚（可选）
+        int = 13, -- INT引脚（可选）
+        fre = ft6336u.I2C_FREQ_400K, -- I2C频率
         x_limit = 320,
         y_limit = 320,
         swapxy = true,
         mirror_x = false,
         mirror_y = true
-    })
-
-    for i = 1, #vButton do
-        local color = 0x0
-        if i==1 then
-            color = colorList[i]
-        end
-        lcd.fill(vButton[i].x, vButton[i].y, vButton[i].x + vButton[i].w, vButton[i].y + vButton[i].h, color)
+    }
+    ft6336u.init(config)
+    -- 初始化触摸屏
+    local success = ft6336u.init(config)
+    if not success then
+        log.error("ft6336u", "initialization failed")
+        return
     end
 
-    local lastX = -1
-    local lastY = -1
-    while true do
-        sys.wait(10)
-        local x, y, sta = ft6636u.getState()
-        -- log.info("main", "TOUCH READ X,Y,STA", x,y,sta)
-        if (sta == 1) then
-            -- 判断在不在虚拟按钮内部
-            local buttonId = -1
-            for i = 1, #vButton do
-                if (x >= vButton[i].x and x <= vButton[i].x + vButton[i].w and y >= vButton[i].y and y <= vButton[i].y +
-                    vButton[i].h) then
-                    buttonId = i
-                    break
-                end
-            end
-            if buttonId ~= -1 then
-                if (buttonId == 1) then
-                    nowColor = nowColor + 1
-                    if nowColor > #colorList then
-                        nowColor = 1
-                    end
-                    -- 显示当前颜色
-                    local bx = vButton[buttonId].x
-                    local by = vButton[buttonId].y
-                    local bw = vButton[buttonId].w
-                    local bh = vButton[buttonId].h
-                    lcd.fill(bx, by, bx + bw, by + bh, colorList[nowColor])
-                elseif (buttonId == 2) then
-                    lcd.clear()
-                end
-            else
-                if (lastX ~= x or lastY ~= y) and x ~= -1 and y ~= -1 and lastX ~= -1 and lastY ~= -1 then
-                    lcd.drawLine(lastX, lastY, x, y, colorList[nowColor])
-                else
-                    lcd.drawPoint(x, y, colorList[nowColor])
-                end
-                lastX = x
-                lastY = y
-            end
-        else
-            lastX = -1
-            lastY = -1
+    lvgl.init()
+    -- 获取触摸屏信息
+    local info = ft6336u.info()
+    log.info("ft6336u", "chip_id", info.chip_id, "firmware", info.firmware_version)
+    -- 将触摸屏注册到LVGL
+    success = ft6336u.lvgl_register()
+    if not success then
+        log.error("ft6336u", "LVGL registration failed")
+        return
+    end
+
+    log.info("ft6336u", "successfully registered with LVGL")
+
+    local list_demo = {}
+
+    local function event_handler(obj, event)
+        if (event == lvgl.EVENT_CLICKED) then
+            print(string.format("Clicked: %s\n", lvgl.list_get_btn_text(obj)));
         end
     end
+
+    -- Create a list
+    local list1 = lvgl.list_create(lvgl.scr_act(), nil);
+    lvgl.obj_set_size(list1, 160, 200);
+    lvgl.obj_align(list1, nil, lvgl.ALIGN_CENTER, 0, 0);
+
+    -- Add buttons to the list
+    local list_btn;
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_FILE, "New");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_DIRECTORY, "Open");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_CLOSE, "Delete");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_EDIT, "Edit");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_SAVE, "Save");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_BELL, "Notify");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    list_btn = lvgl.list_add_btn(list1, lvgl.SYMBOL_BATTERY_FULL, "Battery");
+    lvgl.obj_set_event_cb(list_btn, event_handler);
+
+    return list_demo
 end)
 
 sys.run()
